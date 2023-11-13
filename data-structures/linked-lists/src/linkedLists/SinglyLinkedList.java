9806bcc68c14be139a9dec494ae5f8fce830ebd5
Anderson Menezes <andersonmenezes.dev@gmail.com>
2023-11-13 00:33:15 -0300

    Implement basic Simply Linked List

---
+++ data-structures/linked-lists/src/linkedLists/SinglyLinkedList.java
@@ -0,0 +1,107 @@
+package linkedLists;
+
+public class SinglyLinkedList<T> {
+    private Node<T> head = null;
+    private Node<T> tail = null;
+    private int size = 0;
+
+    public SinglyLinkedList() {
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public T first() {
+        return head.getElement();
+    }
+
+    public T last() {
+        return tail.getElement();
+    }
+
+    public T addFirst(T element) {
+        Node<T> newHead = new Node<>(element, head);
+        head = newHead;
+        if (isEmpty())
+            tail = head;
+
+        size++;
+
+        return newHead.getElement();
+    }
+
+    public T addLast(T element) {
+        Node<T> newTail = new Node<>(element, null);
+        if (isEmpty())
+            head = newTail;
+        else
+            tail.setNextNode(newTail);
+
+        tail = newTail;
+        size++;
+
+        return newTail.getElement();
+    }
+
+    public T removeFirst() {
+        if (isEmpty()) return null;
+
+        Node<T> elementRemoved = head;
+
+        head = head.getNext();
+        size--;
+
+        if (isEmpty()) {
+            tail = null;
+        }
+
+        return elementRemoved.getElement();
+    }
+
+    public T removeLast() {
+        if (isEmpty()) return null;
+
+        Node<T> nodeRemoved = tail;
+
+        Node<T> currentNode = head;
+
+        while (currentNode.getNext().getNext() != null) {
+            //currentNode represents second last node
+            currentNode = currentNode.getNext();
+        }
+
+        currentNode.setNextNode(null);
+        tail = currentNode;
+        size--;
+
+        return nodeRemoved.getElement();
+    }
+
+
+    private static class Node<T> {
+        private final T element;
+        private SinglyLinkedList.Node<T> nextNode;
+
+        public Node(T element, SinglyLinkedList.Node<T> nextNode) {
+            this.element = element;
+            this.nextNode = nextNode;
+        }
+
+        public T getElement() {
+            return element;
+        }
+
+        public SinglyLinkedList.Node<T> getNext() {
+            return nextNode;
+        }
+
+        public void setNextNode(SinglyLinkedList.Node<T> nextNode) {
+            this.nextNode = nextNode;
+        }
+    }
+}
